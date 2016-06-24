1ed1fd4ce26f303ba3b8c5f361a5e2630239b854
Andrew Stone <andrewstone8@gmail.com>
2016-06-24 19:23:59 +0900

    Update recipe_book.rb

--- lib/recipe_book.rb
+++ lib/recipe_book.rb
@@ -1,8 +1,9 @@
 require 'open-uri'
-require "nokogiri"
-require "pry"
+require 'nokogiri'
+require 'pry'
 
 
 require_relative "recipe_book/version"
-require_relative 'recipe_book/recipe.rb'
 require_relative 'recipe_book/cli.rb'
+require_relative 'recipe_book/recipe.rb'
+
